d3f4dbffd8015854e8592e8ea476fb92dba40106
Bill Columbia <hello@billcolumbia.com>
2012-11-02 11:25:05 -0400

    No really… now I added the Login System

---
+++ login.php
@@ -0,0 +1,5 @@
+<form name="loginForm" method="post" action="secure.php">
+	<input name="myusername" type="text" id="myusername">
+	<input name="mypassword" type="text" id="mypassword">
+	<input type="submit" name="Submit" value="Login">
+</form>
